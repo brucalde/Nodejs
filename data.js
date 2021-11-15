a4a24a1b84f37255621dbc93009e3bca7f0ebdea
brucalde <91205564+brucalde@users.noreply.github.com>
2021-11-15 20:58:25 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -2,14 +2,14 @@
 
 const productos =[
     {
-        categoria ="indumentaria",
-        nombre = "remera",
-        precio = 2000
+        categoria:"indumentaria",
+        nombre: "remera",
+        precio: 2000
     },
     {
-        categoria = "electrodomesticos",
-        nombre =" heladera",
-        precio = 70000
+        categoria: "electrodomesticos",
+        nombre :" heladera",
+        precio: 70000
     }
 ];
 
